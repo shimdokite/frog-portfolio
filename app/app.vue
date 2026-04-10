34e39b05dcbb12c8323daa69ad3c3c82da43fa34
shimdokite <shimdokite@gmail.com>
2026-04-10 22:25:34 +0900

    style: 전체 배경색 수정

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="min-h-screen bg-[#083647]">
+  <div class="app-pond-bg min-h-screen">
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+.app-pond-bg {
+  position: relative;
+  overflow: hidden;
+  background: linear-gradient(200deg, #0c3640 10%, #0a353e 40%, #082e36 100%);
+}
+</style>
