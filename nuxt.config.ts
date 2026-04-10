8712c468e0170677d5d0aca6fa78ab37edf9dd43
shimdokite <shimdokite@gmail.com>
2026-04-10 22:35:58 +0900

    chore: nuxt/font 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/fonts'],
   css: ['~/assets/css/main.css']
-})
+});
